5cd32e8ac566a10ba06781ffa6febe36fccecc8b
Samuel Arjuna Queen Bernard <arjunaaber2@gmail.com>
2023-03-10 10:23:34 +0700

    v1.0

--- tugas_commit.py
+++ tugas_commit.py
@@ -18,17 +18,3 @@ user = [
     "tanggal_lahir": "1884-10-07"
   }
 ]
-
-nama = input("Masukkan nama: ")
-nik = input("Masukkan NIK: ")
-jenis_kelamin = input("Masukkan jenis kelamin (male/female): ")
-tanggal_lahir = input("Masukkan tanggal lahir (yyyy-mm-dd): ")
-
-user.append({
-  "nama": nama,
-  "nik": nik,
-  "jenis_kelamin": jenis_kelamin,
-  "tanggal_lahir": tanggal_lahir
-})
-
-print("Data berhasil ditambahkan!") 
